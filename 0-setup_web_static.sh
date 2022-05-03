8b1035c8236088fd0cbc5b2e49b4940ca9fcd922
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-05-03 15:43:57 +0200

    added task 0 of deploy static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Hello Holberton !" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '47i\\t location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sudo service nginx restart
