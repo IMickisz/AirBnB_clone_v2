78507b2d765d6334bc9991b5c13eda39e12888c6
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-05-05 13:50:51 +0200

    added task 5 (advanced)

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,11 @@
+# Task #0 (sets up your web servers for the deployment of web_static) but by using Puppet
+
+exec { '/usr/bin/env apt-get -y update': }
+-> exec { '/usr/bin/env apt-get -y install nginx': }
+-> exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/': }
+-> exec { '/usr/bin/env mkdir -p /data/web_static/shared/': }
+-> exec { '/usr/bin/env echo "Hello Holberton !" > /data/web_static/releases/test/index.html': }
+-> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current': }
+-> exec { '/usr/bin/env chown -R ubuntu:ubuntu /data/': }
+-> exec { '/usr/bin/env sed -i "47i location /hbnb_static { alias /data/web_static/current/; }" /etc/nginx/sites-available/default': }
+-> exec { '/usr/bin/env service nginx restart': }
