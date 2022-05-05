525f671f40207deda7c6eb7467e302aaa3b3c5e4
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-05-05 14:08:39 +0200

    added task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Module that contains the do_pack and do_deploy functions
+"""
+
+from fabric.api import local, put, run, env
+from time import strftime as time
+from os import path
+env.hosts = ['35.196.253.106', '34.148.114.20']
+
+
+def do_pack():
+    """Function that generates a .tgz archive from the web_static folder"""
+    if path.exists("versions/") is False:
+        local("mkdir versions/")
+    try:
+        file_path = "versions/web_static_{}{}{}{}{}{}.tgz".format(time("%Y"),
+                                                                  time("%m"),
+                                                                  time("%d"),
+                                                                  time("%H"),
+                                                                  time("%M"),
+                                                                  time("%S"))
+        local("tar -cvzf {} web_static".format(file_path))
+        return file_path
+    except Exception:
+        return None
+
+def do_deploy(archive_path):
+    """Function that distributes an archive to your web servers"""
+    if path.exists(archive_path) is False:
+        return False
+    try:
+        p = "/data/web_static/releases"
+        file_name = archive_path.split("/")
+        put("{}/{}".format(file_name[0], file_name[1]),
+            "/tmp/{}".format(file_name[1]))
+        del_ext = file_name[1].split(".")
+        run("sudo mkdir -p {}/{}/".format(p, del_ext[0]))
+        run("tar -xzf /tmp/{} -C {}/{}/".format(file_name[1], p, del_ext[0]))
+        run("rm /tmp/{}".format(file_name[1]))
+        run("mv {}/{}/web_static/* {}/{}/".format(p, del_ext[0], p, del_ext[0]))
+        run("rm -rf {}/{}/web_static".format(p, del_ext[0]))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/{}/ /data/web_static/current".format(p, del_ext[0]))
+        return True
+    except Exception:
+        return False
