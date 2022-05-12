ca2025a725540d0cd182b4f6dd40e3589b91573b
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-05-12 11:44:33 +0200

    added task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Returns 'Hello HBNB!' at the root route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """Returns 'HBNB' at the /hbnb route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_route(text):
+    """Returns “C” followed by the value of the text variable at the
+    /c/<test> route"""
+    remove_ = text.replace('_', ' ')
+    return 'C %s' % remove_
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """Returns “python” followed by the value of the text variable or "is cool"
+    by default at the /python/<text> route"""
+    remove_ = text.replace('_', ' ')
+    return 'C %s' % remove_
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """Returns n if it is an interger at /number/<n> route"""
+    if type(n) == int:
+        return '%i is a number' % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_route(n):
+    """Returns a template if n is an interger at /number_template//<n> route"""
+    if type(n) == int:
+        return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
